b67b5ea113e9c22b9caef639782a6687d0edf064
Sungjun <opr99@naver.com>
2020-08-18 18:31:28 +0900

    update

--- app/main.js
+++ app/main.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 4000
 const compression = require('compression')
 const helmet = require('helmet')
 const bodyParser = require('body-parser')
