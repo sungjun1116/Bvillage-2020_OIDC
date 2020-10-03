31c1f24942bc82d2390e9d13396e8d01bf606612
Sungjun <opr99@naver.com>
2020-10-03 15:50:39 +0900

    dfdf

--- app/routes/post.js
+++ app/routes/post.js
@@ -2,7 +2,7 @@
 const express = require('express')
 const router = express.Router()
 
-router.post('/post', (req, res, next) => {
+router.post('/', (req, res, next) => {
   const post = req.body;
   // const city = post.city;
   res.send(`성공했다 이자식아: ${post}`);
