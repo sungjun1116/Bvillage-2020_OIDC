152a93d09928658ac1f2fab38a5b97e81dbdd4ab
Sungjun <opr99@naver.com>
2020-10-03 15:38:13 +0900

    update

--- app/main.js
+++ app/main.js
@@ -5,6 +5,7 @@ const compression = require('compression')
 const helmet = require('helmet')
 const bodyParser = require('body-parser')
 const indexRouter = require('./routes/index')
+const postRouter = require('./routes/post')
 const cityRouter = require('./routes/city')
 const exchangeRouter = require('./routes/exchange')
 const communityRouter = require('./routes/community')
@@ -19,6 +20,7 @@ app.use(helmet())
 
 // Router
 app.use('/', indexRouter);
+app.use('/post', postRouter);
 app.use('/city', cityRouter);
 app.use('/exchange', exchangeRouter);
 app.use('/community', communityRouter);
---
+++ app/routes/post.js
@@ -0,0 +1,11 @@
+'use strict';
+const express = require('express')
+const router = express.Router()
+
+router.post('/post', (req, res, next) => {
+  const post = req.body;
+  // const city = post.city;
+  res.send(`성공했다 이자식아: ${post}`);
+});
+
+module.exports = router;
